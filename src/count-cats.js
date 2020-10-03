127b0a72880fb149678d30cf3360cb22e32c3471
ilia844 <w5332660@mail.ru>
2020-10-03 15:23:26 +0300

    fix: update 'count-cats'

--- src/count-cats.js
+++ src/count-cats.js
@@ -6,9 +6,9 @@ module.exports = function countCats(matrix) {
     return 0;
   } else {
     let count = 0;
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; i < matrix[i].length; j++) {
-        if (matrix[i][j] === '^^') {
+    for (let subArr of matrix) {
+      for (let item of subArr) {
+        if (item === '^^') {
           count++;
         }
       }
