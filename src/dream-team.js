c31289a4c72989e00ffec532072365997e0dd78d
ilia844 <w5332660@mail.ru>
2020-10-03 16:55:20 +0300

    feat: complete task 'dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members === null || members === undefined) return false;
+  let letterArr = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      letterArr.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  if (letterArr.length === 0) return false;
+  letterArr.sort();
+  return letterArr.join('');
 };
