31637797cee3fb226c7971caab8930da5b97300d
ilia844 <w5332660@mail.ru>
2020-10-04 19:20:14 +0300

    feat: complete task 'transform-array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  let transfromedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' && arr[i + 1] !== undefined) {
+      if(arr[i + 2] === '--discard-prev' || arr[i + 2] === '--double-prev')
+        i += 2;
+      else
+        i++;
+    } else if (arr[i] === '--discard-prev' && arr[i - 1] !== undefined) {
+        transfromedArr.pop();
+    } else if (arr[i] === '--double-next' && arr[i + 1] !== undefined) {
+        transfromedArr.push(arr[i+1]);
+    } else if (arr[i] === '--double-prev' && arr[i - 1] !== undefined) {
+        transfromedArr.push(arr[i-1]);
+    } else {
+      if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-prev' && arr[i] !== '--double-next') {
+        transfromedArr.push(arr[i]);
+      }
+    }
+  }
+  return transfromedArr;
+  
 };
